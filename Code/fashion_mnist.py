7e48f1992495c5c0e9fd2f303e579069eb981609
The Roy Akash <theakashaiims@gmail.com>
2019-11-23 16:27:20 +0530

    Update fashion_mnist.py

--- Code/fashion_mnist.py
+++ Code/fashion_mnist.py
@@ -68,36 +68,36 @@ np.argmax(predictions[0])
 test_labels[0]
 
 def plot_image(i, predictions_array, true_label, img):
-  predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
-  plt.grid(False)
-  plt.xticks([])
-  plt.yticks([])
+    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
   
-  plt.imshow(img, cmap=plt.cm.binary)
+    plt.imshow(img, cmap=plt.cm.binary)
   
-  predicted_label = np.argmax(predictions_array)
-  # Showing wrong predictions in red:
-  if predicted_label == true_label:
-    color = 'blue'
-  else:
-    color = 'red'
+    predicted_label = np.argmax(predictions_array)
+    # Showing wrong predictions in red:
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
   
-  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
                                 100*np.max(predictions_array),
                                 class_names[true_label]),
                                 color=color)
 
 def plot_value_array(i, predictions_array, true_label):
-  predictions_array, true_label = predictions_array[i], true_label[i]
-  plt.grid(False)
-  plt.xticks([])
-  plt.yticks([])
-  thisplot = plt.bar(range(10), predictions_array, color="#777777")
-  plt.ylim([0, 1])
-  predicted_label = np.argmax(predictions_array)
+    predictions_array, true_label = predictions_array[i], true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
   
-  thisplot[predicted_label].set_color('red')
-  thisplot[true_label].set_color('blue')
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
 
 i = 0
 plt.figure(figsize=(6,3))
@@ -121,8 +121,10 @@ num_cols = 3
 num_images = num_rows*num_cols
 plt.figure(figsize=(2*2*num_cols, 2*num_rows))
 for i in range(num_images):
-  plt.subplot(num_rows, 2*num_cols, 2*i+1)
-  plot_image(i, predictions, test_labels, test_images)
-  plt.subplot(num_rows, 2*num_cols, 2*i+2)
-  plot_value_array(i, predictions, test_labels)
+    plt.subplot(num_rows, 2*num_cols, 2*i+1)
+    plot_image(i, predictions, test_labels, test_images)
+    plt.subplot(num_rows, 2*num_cols, 2*i+2)
+    plot_value_array(i, predictions, test_labels)
+    
+    
 plt.show()
